ea60d961447e890b0917d91f4bea862cb8ca7cb0
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2022-12-03 20:12:45 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -2,44 +2,22 @@
 
 include_once "conection.php";
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-if(empty($dados['Nome'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Nome!</div>"];
-}
-elseif(empty($dados['RA'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo RA!</div>"];
-}
-
-elseif(empty($dados['Email'])){
+if(empty($dados['email'])){
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Email!</div>"];
 }
-
-elseif(empty($dados['Idade'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Idade!</div>"];
+elseif(empty($dados['Nome'])){
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Nome!</div>"];
 }
-
-elseif(empty($dados['Telefone'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Telefone!</div>"];
-}
-
 elseif(empty($dados['endereco'])){
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo endereco!</div>"];
 }
-
-elseif(empty($dados['Sexo'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Sexo!</div>"];
-}
-
 else {
-    $querry_usuario = "INSERT INTO estudantes (Nome, RA, Email, Idade, Telefone, endereco, Sexo) 
+    $querry_usuario = "INSERT INTO usuario (email, psw, psw-repeat) 
     VALUES (:Nome, :RA, :Email, Idade, :Telefone, :endereco, :Sexo)";
     $cad_usuario = $conn->prepare($query_usuario);
-    $cad_usuario->bindParam(':Nome', $dados['Nome']);
-    $cad_usuario->bindParam(':RA', $dados['RA']);
-    $cad_usuario->bindParam(':Email', $dados['Email']);
-    $cad_usuario->bindParam(':Idade', $dados['Idade']);
-    $cad_usuario->bindParam(':Telefone', $dados['Telefone']);
-    $cad_usuario->bindParam(':endereco', $dados['endereco']);
-    $cad_usuario->bindParam(':Sexo', $dados['Sexo']);
+    $cad_usuario->bindParam(':Email', $dados['email']);
+    $cad_usuario->bindParam(':Senha', $dados['psw']);
+    $cad_usuario->bindParam(':endereco', $dados['psw-repeat']);
 
     $cad_usuario->execute();
     if ($cad_usuario->rowCount()) {
