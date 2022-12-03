7b90e4aefb02fbc94ec9e3dcf5a7fff007a7d312
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2022-12-03 20:25:10 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -5,25 +5,37 @@ $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 if(empty($dados['nome'])){
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Nome!</div>"];
 }
+elseif(empty($dados['date'])){
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Data de Nascimento!</div>"];
+}
+elseif(empty($dados['cpf'])){
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo CPF!</div>"];
+}
 elseif(empty($dados['phnumber'])){
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Telefone!</div>"];
 }
 elseif(empty($dados['email'])){
     $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Email!</div>"];
 }
+elseif(empty($dados['user'])){
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Usuario!</div>"];
+}
 elseif(empty($dados['psw'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo Nome!</div>"];
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo senha!</div>"];
 }
 elseif(empty($dados['psw-repeat'])){
-    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo endereco!</div>"];
+    $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Necessário preencher o campo repitir senha!</div>"];
 }
 else {
     $querry_usuario = "INSERT INTO usuario (nome, phnumber, email, psw, psw-repeat) 
     VALUES (:Nome, :RA, :Email, Idade, :Telefone, :endereco, :Sexo)";
     $cad_usuario = $conn->prepare($query_usuario);
     $cad_usuario->bindParam(':Nome', $dados['nome']);
+    $cad_usuario->bindParam(':Data de Nascimento', $dados['date']);
+    $cad_usuario->bindParam(':Cpf', $dados['cpf']);
     $cad_usuario->bindParam(':Telefone', $dados['phnumber']);
     $cad_usuario->bindParam(':Email', $dados['email']);
+    $cad_usuario->bindParam(':Usuário', $dados['user']);
     $cad_usuario->bindParam(':Senha', $dados['psw']);
     $cad_usuario->bindParam(':Repitir Senha', $dados['psw-repeat']);
 
