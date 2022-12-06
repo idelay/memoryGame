c66ef17a4a069a77153a048032f4f5f432bca4e7
idelay <98506951+idelay@users.noreply.github.com>
2022-12-06 02:17:40 -0300

    Update modify.php

--- modify.php
+++ modify.php
@@ -1,1 +1,33 @@
+<?php
 
+$name = $_POST['nome'];
+$email = $_POST['email'];
+$phone = $_POST['telefone'];
+$password = $_POST['password'];
+
+
+$servername = 'localhost';
+$username = 'root';
+$dbname = 'jogo_da_memoria';
+$connect = new mysqli("localhost","root",'',"jogo_da_memoria");
+
+
+if ($connect-> connect_error) {
+       die('connection failed :' .$connect-> $connect_error);
+}
+
+else {
+       $sql = ("INSERT INTO usuario(nome,telefone,email,senha)
+       VALUES ('$name','$phone','$email','$password')");
+}
+
+$sql = mysqli_query($connect, $sql);
+
+
+if ($sql == true) {
+       echo "Usuário salvo.";
+} else {
+       echo "Usuário não foi salvo.";
+}
+
+?> 
