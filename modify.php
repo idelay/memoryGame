b0931f96ddb664e85a23c1a8333f78e680e25815
idelay <98506951+idelay@users.noreply.github.com>
2022-12-06 03:33:30 -0300

    Update modify.php

--- modify.php
+++ modify.php
@@ -1,5 +1,5 @@
 <?php
-
+include('logging.php');
 session_start();
 $name = $_POST['nome'];
 $email = $_POST['email'];
@@ -20,7 +20,7 @@ if ($connect-> connect_error) {
 else {
        $sql = ("UPDATE usuario
        SET nome = $name, telefone = $phone, email = $email, senha = $password
-       WHERE login = $_SESSION['sess_user'];");
+       WHERE login = '" . $_SESSION["sess_user"] ."';");
 }
 
 $sql = mysqli_query($connect, $sql);
