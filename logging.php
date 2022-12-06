25ae3a26d190cdbef3809c472df639e048576361
idelay <98506951+idelay@users.noreply.github.com>
2022-12-06 02:09:49 -0300

    Update logging.php

--- logging.php
+++ logging.php
@@ -8,11 +8,11 @@ if(!empty($_POST['uname']) && !empty($_POST['pass'])) {
     $con=mysqli_connect('localhost','root','') or die(mysqli_error());  
     mysqli_select_db($con,'jogo_da_memoria') or die("Banco de dados não pôde ser conectado");  
   
-    $query=mysqli_query("SELECT * FROM usuario WHERE login='".$user."' AND senha='".$pass."'");  
-    $numrows=mysqli_num_rows($query);  
+    $query=mysqli_query($con,"SELECT * FROM usuario WHERE login='".$user."' AND senha='".$pass."'");  
+    $numrows=mysqli_num_rows($con,$query);  
     if($numrows!=0)
     {  
-    while($row=mysqli_fetch_assoc($query))  
+    while($row=mysqli_fetch_assoc($con,$query))  
     {  
     $dbusername=$row['login'];  
     $dbpassword=$row['senha'];  
